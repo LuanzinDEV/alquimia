fe4c63cef63d6252d620cda1c878ac2a66c54585
Luan Tobias <luantobiasdasilveiracamposdev@gmail.com>
2025-01-05 16:35:02 -0300

    começando projeto

---
+++ index.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/assets/imagens/favicon-32x32.png" type="image/png">
+    <title>Alquimia - eventos</title>  
+</head>
+<body>
+    <header class="nav-menu">
+
+        <div class="computador_menu">
+            <div class="item_esquerdo">
+                <h2>ALQUIMIA</h2>
+            </div>
+
+            <div class="item_direito">
+                <ul>
+                    <li>ACESSE SUA CONTA</li>
+                    <li>CADASTRE-SE</li>   
+                </ul>
+            </div>
+        </div>
+        
+    </header>
+</body>
+</html>
